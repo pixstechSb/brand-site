8c099b4b41b41b8a304c5899cfaa873307c1913a
pixstechSb <sureshbalajia@pixstech.com>
2025-01-25 19:20:21 +0530

    API Uri alteration

--- src/components/CareersPage/services/careers-services.tsx
+++ src/components/CareersPage/services/careers-services.tsx
@@ -2,7 +2,7 @@ import { createHttpClient } from '../../../Services/httpClient';
 import {apiBaseUri} from '../../../utils/constants';
 
 export const listJobs = async ( controller:string,uri:string,experience:string, location:string) => {
-    const httpClient = createHttpClient(`${apiBaseUri}/${controller}/${uri}`)
+    const httpClient = createHttpClient(`${apiBaseUri}${controller}${uri}`)
     try {
       console.log("HttpClient",httpClient)
       const response = await httpClient.post(`${apiBaseUri}${uri}`, {"location": location, "experience": experience} );
